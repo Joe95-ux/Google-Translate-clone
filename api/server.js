69378364d62dd73f1e110f15c9f544a3c024a722
joe95-ux <ogorktabi@gmail.com>
2024-04-17 00:49:26 +0200

    fix chatgpt language detection bug

--- api/server.js
+++ api/server.js
@@ -1,96 +1,95 @@
-const PORT = 4000
-const axios = require('axios').default
-const express = require('express')
-const cors = require('cors')
-const  { OpenAI } =  require("openai");
-require('dotenv').config()
-const app = express()
+const PORT = 4000;
+const axios = require("axios").default;
+const express = require("express");
+const cors = require("cors");
+const { OpenAI } = require("openai");
+require("dotenv").config();
+const app = express();
 
 app.use(cors());
 
 //openai config
 
 const openai = new OpenAI({
-  apiKey: process.env.OPEN_API_KEY
+  apiKey: process.env.OPEN_API_KEY,
 });
 
 app.use(express.json());
 const headers = {
-  'content-type': 'application/x-www-form-urlencoded',
-  'x-rapidapi-host': process.env.RAPID_API_HOST,
-  'x-rapidapi-key': process.env.RAPID_API_KEY,
-}
+  "content-type": "application/x-www-form-urlencoded",
+  "x-rapidapi-host": process.env.RAPID_API_HOST,
+  "x-rapidapi-key": process.env.RAPID_API_KEY,
+};
 
 const lanOptions = {
-  method: 'GET',
-  url:'https://google-translate113.p.rapidapi.com/api/v1/translator/support-languages',
-  headers:headers
-}
-async function getLanguageShort(language){
+  method: "GET",
+  url: "https://google-translate113.p.rapidapi.com/api/v1/translator/support-languages",
+  headers: headers,
+};
+async function getLanguageShort(language) {
   let lan;
-  if(language === "Detect language"){
+  if (language === "Detect language") {
     lan = "Automatic";
-  }else if(language.includes("Detected")){
+  } else if (language.includes("Detected")) {
     lan = language.split(" - ")[0];
-  }else{
+  } else {
     lan = language;
   }
   try {
-    const response = await axios.request(lanOptions)
+    const response = await axios.request(lanOptions);
     const data = response.data;
-    const filteredLan = data.filter(languageObj=>languageObj.language === lan);
+    const filteredLan = data.filter(
+      (languageObj) => languageObj.language === lan
+    );
     return filteredLan[0].code;
-    
   } catch (error) {
     console.log(error);
   }
-
 }
 
-app.get('/languages', async (req, res) => {
+app.get("/languages", async (req, res) => {
   try {
     const response = await axios.request(lanOptions);
-    const arrayData = response.data.map((language)=>{
-      if(language.language === "Automatic"){
-        return "Detect language"
-      }else{
-        return language.language
+    const arrayData = response.data.map((language) => {
+      if (language.language === "Automatic") {
+        return "Detect language";
+      } else {
+        return language.language;
       }
     });
     res.status(200).json(arrayData);
   } catch (err) {
-    console.log(err)
-    res.status(500).json({ message: err })
+    console.log(err);
+    res.status(500).json({ message: err });
   }
-})
+});
 
-app.get('/translation', async (req, res) => {
+app.get("/translation", async (req, res) => {
   const { textToTranslate, outputLanguage, inputLanguage } = req.query;
   const fromLang = await getLanguageShort(inputLanguage);
   const toLang = await getLanguageShort(outputLanguage);
-  
+
   const options = {
-    method: 'POST',
-    headers:headers,
+    method: "POST",
+    headers: headers,
     data: new URLSearchParams({
       from: fromLang,
       to: toLang,
       text: textToTranslate,
-
-    })
-  }
+    }),
+  };
 
   try {
     const response = await axios.request(
-      'https://google-translate113.p.rapidapi.com/api/v1/translator/text',
+      "https://google-translate113.p.rapidapi.com/api/v1/translator/text",
       options
-    )
-    res.status(200).json(response.data)
+    );
+    res.status(200).json(response.data);
   } catch (err) {
-    console.log(err)
-    res.status(500).json({ message: err })
+    console.log(err);
+    res.status(500).json({ message: err });
   }
-})
+});
 
 // app.get('/detect-language', async (req, res) => {
 //   const {textToTranslate} = req.query;
@@ -112,31 +111,30 @@ app.get('/translation', async (req, res) => {
 //   }
 // })
 
-
 // open ai requests
 
 app.get("/detect-language", async (req, res) => {
   try {
-    const {textToTranslate} = req.query;
+    const { textToTranslate } = req.query;
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: [
         {
-          "role": "system",
-          "content": "Hello! I'm here to help you detect the language of a given text. Please provide the text you want me to analyze. I will only send the language as response and nothing more"
+          role: "system",
+          content:
+            "Hello! I'm here to help you detect the language of a given text. Please provide the text you want me to analyze. I will only send the language as response and nothing more. ",
         },
         {
-          "role": "user",
-          "content": textToTranslate
-        }
-      ]
-      
+          role: "user",
+          content: textToTranslate,
+        },
+      ],
     });
-    res.status(200).json(response.choices[0].message.content)
+    res.status(200).json(response.choices[0].message.content);
   } catch (error) {
     console.error("error", error);
     res.status(500).json({ error: error.message });
   }
 });
 
-app.listen(PORT, () => console.log('Server running on port ' + PORT))
+app.listen(PORT, () => console.log("Server running on port " + PORT));
