0149634615b4088cae017ee7a0cc730d16ba4b84
joe95-ux <ogorktabi@gmail.com>
2024-05-30 23:28:33 +0200

    fix api headers bug

--- api/server.js
+++ api/server.js
@@ -14,6 +14,7 @@ import { translateText } from "./controllers/translateText.js";
 import {
   translateDoc,
   lanOptions,
+  headers,
   generateTranslatedPdf,
   convertDocxToHTML,
   convertHTMLToDocx,
