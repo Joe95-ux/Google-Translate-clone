ddabcb12ecce3affc47bbcfa6656c9fae6125832
joe95-ux <ogorktabi@gmail.com>
2024-06-18 23:17:22 +0200

    edit footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div style={{margin:"3rem auto 0", fontSize:"14px", fontWeight:"normal", textAlign:"center"}}>
-        <p style={{color:"#f5f5f5"}}>Powered by <span style={{color:"#38BDF8"}}>Google Cloud Translation and Chatgpt4</span></p>
+        <p style={{color:"#f5f5f5"}}>Powered by <span style={{color:"#38BDF8"}}>Google Cloud Translation and Chatgpt4o</span></p>
     </div>
   )
 }
