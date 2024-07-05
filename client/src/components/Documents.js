3182943827ca908fe4e72b25833bf30b74c17df9
joe95-ux <ogorktabi@gmail.com>
2024-07-05 23:27:41 +0200

    add .XLS, .PPT support

--- client/src/components/Documents.js
+++ client/src/components/Documents.js
@@ -117,7 +117,7 @@ const Documents = ({ fromLanguage, toLanguage }) => {
         <div className="container-fluid">
           <div className="drop-container">
             <div {...getRootProps()} className="drop-area">
-              <input {...getInputProps()} accept=".pdf, .doc, .docx, .pptx, .xlsx"/>
+              <input {...getInputProps()} accept=".pdf, .doc, .docx, PPT, .pptx, XLS, .xlsx"/>
               {isDragActive ? (
                 <p>Drop the file here...</p>
               ) : (
@@ -138,7 +138,7 @@ const Documents = ({ fromLanguage, toLanguage }) => {
                   id="file-upload"
                   type="file"
                   onChange={handleFileUpload}
-                  accept=".pdf, .doc, .docx, .pptx, .xlsx"
+                  accept=".pdf, .doc, .docx, .pptx, XLS, PPT, .xlsx"
                 />
               </div>
               <p>Supported file types: .docx, .pdf, .pptx, .xlsx</p>
