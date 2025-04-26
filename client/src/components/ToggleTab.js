40368071623ccc1700dd8c36ce2d0659110d12b9
joe95-ux <ogorktabi@gmail.com>
2025-04-27 01:55:32 +0200

    add context tab tooltip

--- client/src/components/ToggleTab.js
+++ client/src/components/ToggleTab.js
@@ -1,24 +1,39 @@
+import { useState } from "react";
+import { BsInfoSquare } from "react-icons/bs";
+
 export default function ToggleTab({ isContext, setIsContext }) {
+  const [showTooltip, setShowTooltip] = useState(false);
   return (
     <div className="switch-tab-wrapper">
-      <div className="switch-tab-header">
-      <h3>Provide Context</h3>
-      <p>Turn on or off to provide context to translations with AI</p>
-      </div>
       <div className="toggle-container">
         <button
           className={`toggle-option ${!isContext ? "active" : ""}`}
           onClick={() => setIsContext(false)}
         >
-          OFF
+          CONTEXT OFF
         </button>
         <button
           className={`toggle-option ${isContext ? "active" : ""}`}
           onClick={() => setIsContext(true)}
         >
-          ON
+          CONTEXT ON
         </button>
         <div className={`toggle-slider ${isContext ? "right" : "left"}`}></div>
+      </div>
+      <div className="toggle-title">
+      <button
+        className="info-icon"
+        onClick={() => setShowTooltip(!showTooltip)}
+        aria-label="More information"
+      >
+        <BsInfoSquare size={16} style={{color:"#38BDF8"}}/>
+      </button>
+      {showTooltip && (
+        <div className="tooltip">
+          <p>Turn on or off to provide context to translations with AI</p>
+          <div className="tooltip-arrow"></div>
+        </div>
+      )}
       </div>
     </div>
   );
